69e722ec443c4c9c7fc397fa495e4180efe151b4
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2021-12-10 22:29:50 +0100

    Creating user object

--- script.js
+++ script.js
@@ -7,19 +7,55 @@ const message = document.getElementById("message");
 // Event Listener
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   // Using Constraint API
   isValid = form.checkValidity();
   // Style main message for an error
-  message.textContent = "Please fill out all fields.";
-  message.style.color = "red";
+  if (!isValid) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    return;
+  }
+  // Check to see if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red";
+    return;
+  }
+  //If form is valid and passwords match
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
 }
 
 function processFormData(e) {
   e.preventDefault();
   //Validate Form
   validateForm();
+  // Submit Data if Valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
 }
 
 form.addEventListener("submit", processFormData);
